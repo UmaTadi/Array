ffe14dfa963c292f8bc99efbe0b0a43b22b4ae68
UmaTadi <118440359+UmaTadi@users.noreply.github.com>
2022-11-29 20:39:55 +0530

    commit

---
+++ src/Swapping/Mytest.java
@@ -0,0 +1,12 @@
+package Swapping;
+
+public class Mytest {
+
+	public static void main(String[] args) {
+		int a=10; 
+        int b=20;
+        System.out.println("before swaping values are:"+a+"--"+b);
+
+	}
+
+}
